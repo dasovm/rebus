ea868ae3993508eca75a8f8ccc0da072dff817a8
Marcus <mabe.cth@gmail.com>
2018-02-26 01:01:33 +0100

    Added super basic gif fetching

--- giphy-integration/index.js
+++ giphy-integration/index.js
@@ -1,3 +1,20 @@
-function requestGifs(text) {
-    return text;
+/* eslint-disable no-console */
+const gphApiClient = require('giphy-js-sdk-core');
+
+const client = gphApiClient('9X6cjVQ00kOzDF9nxvCh27qNu9azT9vQ');
+
+// Gif search by text
+function requestGifs(text, limit) {
+  const gifArray = [];
+  client.search('gifs', { q: text, limit })
+    .then(response => {
+      response.data.forEach(gifObject => {
+        console.log(gifObject);
+        gifArray.push(gifObject);
+      });
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+  return gifArray;
 }
