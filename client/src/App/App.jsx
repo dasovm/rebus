affede487c3567710507b00c9fd82dccf3de845f
David Söderberg <dasovm@gmail.com>
2018-02-28 11:03:18 +0100

    Added router and material

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import styles from './App.module.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className={styles.App}>
         <header className={styles.header}>
-          <img src={logo} className={styles.logo} alt="logo" />
           <h1 className={styles.title}>Welcome to Rebus chat app. Is nice.</h1>
         </header>
         <p className={styles.intro}>
           To get started, edit <code>src/App.jsx</code> and save to reload.
         </p>
       </div>
+      </Router>
     );
   }
 }
