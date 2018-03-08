04cded8c24a5a121c60df7ba71a8a4b322c0bf5c
David Söderberg <dasovm@gmail.com>
2018-03-08 11:34:41 +0100

    Home channel transitions

--- client/src/Home/HomeChannelGrid.jsx
+++ client/src/Home/HomeChannelGrid.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import Grow from 'material-ui/transitions/Grow';
 import styles from './HomeChannelGrid.module.css';
 import Loading from '../Loading/Loading';
 
@@ -10,10 +11,12 @@ function HomeChannelGrid({ loading, channels }) {
   else {
     return (
       <div className={styles.channelList}>
-        {channels.map(channel => 
-        <Link key={`link-${channel._id}`} to={`/channel/${channel._id}`} className={styles.channelLink}>
-          <div key={`grid-${channel._id}`} className={styles.channelCard}>{channel.name}</div>
-        </Link>
+        {channels.map((channel, index) => 
+          <Grow in={!loading} {...(!loading ? { timeout: (index + 1) * 500 } : {})}>
+            <Link key={`link-${channel._id}`} to={`/channel/${channel._id}`} className={styles.channelLink}>
+              <div key={`grid-${channel._id}`} className={styles.channelCard}>{channel.name}</div>
+            </Link>
+          </Grow>
         )}
       </div>
     );
